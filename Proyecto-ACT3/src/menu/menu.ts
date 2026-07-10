fcbbad2ce39e651fb69902dd122d6adb9190506b
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-09 20:33:57 -0600

    refactor: se ha implementado el async/await al menu principal

--- Proyecto-ACT3/src/menu/menu.ts
+++ Proyecto-ACT3/src/menu/menu.ts
@@ -2,32 +2,37 @@ import { rl } from "../utils/readline"
 import { menuCliente } from "./menuCliente"
 import { menuProducto } from "./menuProducto"
 
-export function menu() {
-    console.log("\n|***--------  MENU PRINCIPAL  ---------***|")
-    console.log("|------------------------------------------|")
-    console.log("|***- 1. menu cliente               ----***|")
-    console.log("|***- 2. menu producto              ----***|")
-    console.log("|***- 0. salir                      ----***|")
-    console.log("|------------------------------------------|")
+export async function menu() {
+    let salir = false;
 
-    rl.question("ingrese una opcion: ", (opcion) => {
+    while (!salir) {
+        console.log("\n|***--------  MENU PRINCIPAL  ---------***|")
+        console.log("|------------------------------------------|")
+        console.log("|***- 1. menu cliente               ----***|")
+        console.log("|***- 2. menu producto              ----***|")
+        console.log("|***- 0. salir                      ----***|")
+        console.log("|------------------------------------------|")
+
+        const opcion = await rl.question("ingrese una opcion: ");
+
         switch (opcion) {
             case "1":
-                menuCliente();
+                await menuCliente();
                 break;
 
             case "2":
-                menuProducto();
+                await menuProducto();
                 break;
 
             case "0":
                 console.log("adios");
-                rl.close();
+                salir = true;
                 break;
 
             default:
                 console.log("La opcion no existe.");
-                menu();
         }
-    });
+    }
+
+    rl.close();
 }
