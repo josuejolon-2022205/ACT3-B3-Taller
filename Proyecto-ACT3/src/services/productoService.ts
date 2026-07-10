10d3e57cc5aa7c415f67dece4a299a6337cb3924
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-09 19:10:40 -0600

    refactor: se ha implementado async/await al service de producto

--- Proyecto-ACT3/src/services/productoService.ts
+++ Proyecto-ACT3/src/services/productoService.ts
@@ -1,65 +1,52 @@
 import { Producto } from "../models/producto";
-import { producto } from "../data/producto";
-import { estado_venta } from "../models/estado_venta";
-import { categoria_producto } from "../models/categoriaProducto";
-const estadosValidos: estado_venta[] = ["pagado", "pendiente", "cancelada"];
-const categoriasValidas: categoria_producto[] = ["Electrico", "electronico", "de hogar", "comida", "informatico", "escolar"];
+import { categoria_producto } from "../types/categoriaProducto";
+import { Cliente } from "../models/clientes";
+import { listarCliente } from "./clientesService";
+import { leerProductos } from "../utils/reader";
+import { escribirClientes, escribirProductos } from "../utils/writer";
 
-export function listarProducto(): Producto[] {
-    return producto;
+
+export async function listarProducto(): Promise<Cliente[] | string> {
+    return listarCliente();
 }
 
-export function buscarProducto(id: number): Producto | undefined {
-    return producto.find(p => p.id_producto === id);
+export async function buscarProducto(id: number): Promise<Producto | null>{
+    const productos: Producto[] = await leerProductos();
+    return productos.find(p => p.id_producto === id) || null;
 }
 
-export function crearProducto(nuevoProducto: Producto): Producto | string  {
-    if(!nuevoProducto.id_producto) {
-        nuevoProducto.id_producto = producto.length + 1;
-    }
-
-    if(nuevoProducto.precio <= 0)
-        return "el precio no puede ser menor o igual a cero"
-
-    if(nuevoProducto.cantidad < 0)
-        return "la cantidad no puede ser negativa "
-
-    if (!estadosValidos.includes(nuevoProducto.estado_venta))
-        return "el estado de venta no es valido";
-
-    if(!categoriasValidas.includes(nuevoProducto.categoria))
-        return "la categoria no es valida"
-
-    producto.push(nuevoProducto);
-    return nuevoProducto;
+export async function crearProducto(nuevoProducto: Producto): Promise<void>  {
+    const productos: Producto[] = await leerProductos();
+    productos.push(nuevoProducto);
+    await escribirProductos(productos);
 }
 
-export function actualizarProducto(id: number, ActualizarDatos: Partial<Producto>): Producto | string {
-    const producto = buscarProducto(id);
-
-    if(ActualizarDatos.precio !== undefined && ActualizarDatos.precio <= 0)
-        return "el precio no puede ser menor o igual a cero"
-
-    if(ActualizarDatos.cantidad !== undefined && ActualizarDatos.cantidad < 0)
-        return "la cantidad no puede ser negativa"
+export async function actualizarProducto(id: number, ActualizarDatos: Promise<Producto | null>) {
+      const productos: Producto[] = await leerProductos();
+      if(id <= 0 ) return false;
+      
+      const index = productos.findIndex(p => p.id_producto === id);
+      if(index === -1){
+        return false;
+      }
     
-    if (ActualizarDatos.estado_venta !== undefined && !estadosValidos.includes(ActualizarDatos.estado_venta))
-        return "el estado de venta no es valido";
-
-    if(ActualizarDatos.categoria !== undefined && !categoriasValidas.includes(ActualizarDatos.categoria))
-        return "la categoria no es valida"
-
-    if (!producto) return "Producto no existe";
-    Object.assign(producto, ActualizarDatos);
-    return producto;
+      productos[index] = { ...productos[index], ...actualizarProducto};
+      await escribirProductos;
+      return true;
 }
 
-export function eliminarProductoPorId(id: number): void {
-    if (producto.length > 0) {
-        const nuevosProductos = producto.filter(p => p.id_producto !== id);
-        producto.length = 0;
-        producto.push(...nuevosProductos);
-    }
+export async function eliminarProductoPorId(id: number): Promise<boolean> {
+      const productos: Producto[] = await leerProductos();
+      if(id <= 0 ) return false;
+      
+      const index = productos.findIndex(p => p.id_producto === id);
+      if(index === -1){
+        return false;
+      }
+    
+      productos.splice(index, 1)
+      await escribirProductos;
+      return true;
 }
 
 export const calcularSubtotal = (montos: number[]): number =>
@@ -74,24 +61,26 @@ export const calcularTotalFinal = (subtotal: number, iva: number): number =>
     subtotal + iva;
 
 
-export function calcularVentaProducto(id: number, IVA: number = 0.12): void {
-    const productoEncontrado = producto.find(p => p.id_producto === id);
+export async function calcularVentaProducto(id: number, IVA: number = 0.12): Promise<void> {
+    
+    const data = await leerProductos();
+    const productos: Producto[] = typeof data === "string" ? [] : data;
+    const producto = productos.find(p => p.id_producto === id);
 
-    if (!productoEncontrado) {
-        console.log("el producto no existe");
+    if (!producto) {
+        console.log("El producto no existe.");
         return;
     }
-    const montos = [productoEncontrado.precio];
 
-    const subtotal = calcularSubtotal(montos);
+    const subtotal = calcularSubtotal([producto.precio]);
     const iva = calcularIVA(subtotal, IVA);
     const total = calcularTotalFinal(subtotal, iva);
 
-    console.log("|----- resumen de venta ------|")
-    console.log("|-- ID:" + productoEncontrado.id_producto)
-    console.log("|-- subtotal: " + subtotal)
-    console.log("|-- iva: " + iva)
-    console.log("|-- total: " + total)
+    console.log("|----- resumen de venta ------|");
+    console.log("|-- ID: " + producto.id_producto);
+    console.log("|-- subtotal: " + subtotal);
+    console.log("|-- iva: " + iva);
+    console.log("|-- total: " + total);
 }
 
 
