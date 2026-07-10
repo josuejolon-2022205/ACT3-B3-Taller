69a19faa23108d934a8eac6ed6e93c3b540bdaea
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-09 20:53:57 -0600

    fix: se corrigio la escritura de algunos objetos en el crud de clientes

--- Proyecto-ACT3/src/services/clientesService.ts
+++ Proyecto-ACT3/src/services/clientesService.ts
@@ -4,9 +4,8 @@ import { escribirClientes } from "../utils/writer";
 import { validarCliente } from "../utils/validaciones"
 
 export async function listarCliente(): Promise<Cliente[] | string> {
-  return listarCliente();
+  return leerClientes();
 }
-
 
 export async function buscarCliente(id: number): Promise<Cliente | null> {
   const clientes: Cliente[] = await leerClientes();
@@ -14,38 +13,36 @@ export async function buscarCliente(id: number): Promise<Cliente | null> {
 }
 
 export async function crearCliente(nuevoCliente: Cliente): Promise<void> {
+  validarCliente(nuevoCliente);
   const clientes: Cliente[] = await leerClientes();
   clientes.push(nuevoCliente);
   await escribirClientes(clientes);
-
 }
 
-export async function actualizarCliente(id: number, actualizarDatos: Cliente): Promise<boolean> {
-  validarCliente(actualizarDatos);
+export async function actualizarCliente(id: number, actualizarDatos: Partial<Cliente>): Promise<boolean> {
   const clientes: Cliente[] = await leerClientes();
-  if(id <= 0 ) return false;
-  
+  if (id <= 0) return false;
+
   const index = clientes.findIndex(c => c.id_cliente === id);
-  if(index === -1){
+  if (index === -1) {
     return false;
   }
 
-  clientes[index] = { ...clientes[index], ...actualizarDatos};
+  clientes[index] = { ...clientes[index], ...actualizarDatos };
   await escribirClientes(clientes);
   return true;
 }
 
-
 export async function eliminarClientePorId(id: number): Promise<boolean> {
   const clientes: Cliente[] = await leerClientes();
-  if(id <= 0 ) return false;
-  
+  if (id <= 0) return false;
+
   const index = clientes.findIndex(c => c.id_cliente === id);
-  if(index === -1){
+  if (index === -1) {
     return false;
   }
 
-  clientes.splice(index, 1)
-  await escribirClientes;
+  clientes.splice(index, 1);
+  await escribirClientes(clientes);
   return true;
 }
