6f9831ca24fe597d17eb0c58290bdce20578576a
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-10 18:09:26 -0600

    style: se aplico formato/tabulaciones al service de clientes

--- Proyecto-ACT3/src/services/clientesService.ts
+++ Proyecto-ACT3/src/services/clientesService.ts
@@ -4,46 +4,46 @@ import { escribirClientes } from "../utils/writer";
 import { validarCliente } from "../utils/validaciones"
 
 export async function listarCliente(): Promise<Cliente[] | string> {
-  return leerClientes();
+    return leerClientes();
 }
 
 export async function buscarCliente(id: number): Promise<Cliente | null> {
-  const clientes: Cliente[] = await leerClientes();
-  return clientes.find(c => c.id_cliente === id) || null;
+    const clientes: Cliente[] = await leerClientes();
+    return clientes.find(c => c.id_cliente === id) || null;
 }
 
 export async function crearCliente(nuevoCliente: Cliente): Promise<void> {
-  validarCliente(nuevoCliente);
-  const clientes: Cliente[] = await leerClientes();
-  nuevoCliente.id_cliente = clientes.length > 0 ? Math.max(...clientes.map(c => c.id_cliente)) + 1 : 1;
-  clientes.push(nuevoCliente);
-  await escribirClientes(clientes);
+    validarCliente(nuevoCliente);
+    const clientes: Cliente[] = await leerClientes();
+    nuevoCliente.id_cliente = clientes.length > 0 ? Math.max(...clientes.map(c => c.id_cliente)) + 1 : 1;
+    clientes.push(nuevoCliente);
+    await escribirClientes(clientes);
 }
 
 export async function actualizarCliente(id: number, actualizarDatos: Partial<Cliente>): Promise<boolean> {
-  const clientes: Cliente[] = await leerClientes();
-  if (id <= 0) return false;
+    const clientes: Cliente[] = await leerClientes();
+    if (id <= 0) return false;
 
-  const index = clientes.findIndex(c => c.id_cliente === id);
-  if (index === -1) {
-    return false;
-  }
+    const index = clientes.findIndex(c => c.id_cliente === id);
+    if (index === -1) {
+      return false;
+    }
 
-  clientes[index] = { ...clientes[index], ...actualizarDatos };
-  await escribirClientes(clientes);
-  return true;
+    clientes[index] = { ...clientes[index], ...actualizarDatos };
+    await escribirClientes(clientes);
+    return true;
 }
 
 export async function eliminarClientePorId(id: number): Promise<boolean> {
-  const clientes: Cliente[] = await leerClientes();
-  if (id <= 0) return false;
+    const clientes: Cliente[] = await leerClientes();
+    if (id <= 0) return false;
 
-  const index = clientes.findIndex(c => c.id_cliente === id);
-  if (index === -1) {
-    return false;
-  }
+    const index = clientes.findIndex(c => c.id_cliente === id);
+    if (index === -1) {
+      return false;
+    }
 
-  clientes.splice(index, 1);
-  await escribirClientes(clientes);
-  return true;
+    clientes.splice(index, 1);
+    await escribirClientes(clientes);
+    return true;
 }
